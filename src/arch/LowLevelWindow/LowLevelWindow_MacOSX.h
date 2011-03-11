3b9aec5e64ced7daacf534edb5a3c07dfefb3e19
Jason Felds <jafelds@gmail.com>
2011-03-10 23:36:41 -0500

    xcode4 branch: LowLevelWindow compiles again.
    Note: had to comment out a typedef that was previously
    recommended to stay uncommented. Will need testing.

--- src/arch/LowLevelWindow/LowLevelWindow_MacOSX.h
+++ src/arch/LowLevelWindow/LowLevelWindow_MacOSX.h
@@ -8,7 +8,7 @@
 typedef const struct __CFDictionary *CFDictionaryRef;
 /* XXX: This was changed to a uint32_t later and its header file cannot be included
  * since Style conflicts. Ugh. */
-typedef struct _CGDirectDisplayID *CGDirectDisplayID;
+//typedef struct _CGDirectDisplayID *CGDirectDisplayID;
 
 class LowLevelWindow_MacOSX : public LowLevelWindow
 {
